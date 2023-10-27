e108b267540dcc0dd28a330c89db55c7728ea62d
hjlee <heunjong.lee@gmail.com>
2023-10-27 02:13:10 -0400

    designed staff_db schema, added seed data

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS staff_db;
+CREATE DATABASE staff_db;
+
+USE staff_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (30) NOT NULL
+); 
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id) 
+    -- ON DELETE (add later)
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT, FOREIGN KEY (role_id) REFERENCES roles(id),
+    manager_id INT, FOREIGN KEY (manager_id) REFERENCES employees(id)
+    -- ON DELETE (add later)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO departments (name)
+VALUES ("rap line"),
+       ("vocal line");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("koya", 19940912, 1),
+       ("shooky", 19930309, 1), 
+       ("mang", 19940218, 1),
+       ("rj", 19921204, 2),
+       ("chimmy", 19951013, 2),
+       ("tata", 19951230, 2),
+       ("kooky", 19970901, 1);              
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("namjoon", "kim", 1, NULL),
+       ("yoongi", "min", 2, 1),
+       ("hoseok", "jung", 3, 1),
+       ("seokjin", "kim", 4, NULL),
+       ("jimin", "park", 5, 4),
+       ("taehyung", "kim", 6, 4),
+       ("jungkook", "jeon", 7, 4); 
+       
+    
+
